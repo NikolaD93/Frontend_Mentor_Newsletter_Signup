7c7518499495bfc64e8c9e19d674967bc0768d45
Nikola Dojcinovic <goonergalactico@gmail.com>
2023-06-16 22:52:12 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-tomato font-bold">Hello World</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        tomato: "hsl(4, 100%, 67%)",
+        darkSlateGray: "hsl(235, 18%, 26%)",
+        grey: "hsl(231, 7%, 60%)",
+      },
+      screens: {
+        sm: "376px",
+        md: "768px",
+        lg: "1440px",
+      },
+    },
+  },
+  plugins: [],
+};
